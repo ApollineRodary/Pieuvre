94f85f126e88f9017ce15998f5aead93b91974f3
thervier <thibault.hervier@ens-lyon.fr>
2023-05-01 15:23:33 +0200

    Alpha conversion

--- lib/lambda.ml
+++ lib/lambda.ml
@@ -41,4 +41,41 @@ let alpha_convert_fixed (m : lam) (x : var) (x' : var) : lam =
             else
                 m
         | Exf (n, t) -> Exf (aux b n x x', t)
-    in aux false m x x' 
+    in aux false m x x'
+
+let find_fresh_variable (x : var) (env : (var*var) list) : var =
+    let rec aux (i : int) (x : var) (env : (var*var) list) : var = match env with
+        | [] -> x ^ (string_of_int i)
+        | (y', y)::q -> 
+            if y' = x then 
+                begin
+                    match int_of_string_opt (String.sub y (String.length x) (String.length y - String.length x)) with
+                        | Some i' -> aux (i'+1) x q
+                        | None -> aux i x q
+                end
+                
+            else
+                aux i x q
+    in aux 0 x env 
+        
+
+ let alpha_convert (m : lam) (x : var) : lam =
+    let rec aux (m : lam) (x : var) (env : (var*var) list) = match m with
+        | Abstraction (y, t, n) ->
+            if x != y then
+                Abstraction (y, t, aux n x env)
+            else
+                let x' = find_fresh_variable x env in
+                Abstraction (x', t, aux n x ((x, x')::env))
+        | Application (m, n) -> Application ((aux m x env), (aux n x env))
+        | Var y -> 
+            begin
+                match List.assoc_opt y env with
+                    | Some y' -> Var y'
+                    | None -> Var y
+            end
+        
+        | Exf (n, t) -> Exf (aux n x env, t)
+    
+    in aux m x []
+        

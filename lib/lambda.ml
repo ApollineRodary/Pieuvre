0ef21212701f302b5ed92af474e24a1017c1ccd5
thervier <thibault.hervier@ens-lyon.fr>
2023-05-01 13:24:35 +0200

    alpha conversion with fixed name at the end

--- lib/lambda.ml
+++ lib/lambda.ml
@@ -22,3 +22,23 @@ let rec print_lam (l:lam): unit = match l with
         print_lam l;
         print_string (":" ^ t ^ ")")
     end
+
+let alpha_convert_fixed (m : lam) (x : var) (x' : var) : lam = 
+    let rec aux b m x x' = match m with (*b represents if the variable we want to alpha-convert is bound or not*)
+        | Abstraction (y, t, n) -> 
+            if x != y then 
+                Abstraction (y, t, aux b n x x')
+            else if b then
+                m
+            else
+                Abstraction (x', t, aux true n x x')
+        | Application (m, n) -> Application ((aux b m x x'), (aux b n x x'))
+        | Var y ->
+            if y != x then
+                m
+            else if b then
+                Var x'
+            else
+                m
+        | Exf (n, t) -> Exf (aux b n x x', t)
+    in aux false m x x' 

92ab373e077d482e64dddef44f737c4de04d4536
Apolline Rodary <apolline.rodary@ens-lyon.fr>
2023-05-08 17:50:04 +0200

    Update test

--- test/is_alpha_convertible.ml
+++ test/is_alpha_convertible.ml
@@ -1,6 +1,7 @@
 open Lambda
 
-let print_bool b = print_string (if b then "true" else "false")
+
+let print_bool b = print_endline (if b then "true" else "false")
 
 let a = TypeVar "A"
 
@@ -40,14 +41,29 @@ let l3 =
         Abstraction ("v", a, Application (Var ("k"), Var ("v")))
     )
 
+let l4 = Abstraction ("x", a, Var "x")
+let l5 = Abstraction ("y", a, Var "x")
+
+let l6_opt = Parsing.parse_string "fun (x:A) => x"
+let l7_opt = Parsing.parse_string "fun (y:A) => y"
+
+let l6, l7 =
+    match l6_opt, l7_opt with
+    | Some u, Some v -> u, v
+    | _ -> failwith "Parsing error"
+
+let comparisons = [
+    (l1, l2);
+    (l2, l1);
+    (l2, l3);
+    (l3, l1);
+    (l4, l5);
+    (l6, l7)
+]
+
+let print_alpha_convertibility (x, y) = print_bool (is_alpha_convertible x y)
+
 let () = begin
     print_endline "=== Is alpha convertible ===";
-    
-    print_bool (is_alpha_convertible l1 l2);
-    print_newline ();
-    print_bool (is_alpha_convertible l2 l1);
-    print_newline();
-    print_bool (is_alpha_convertible l2 l3);
-    print_newline();
-    print_bool (is_alpha_convertible l3 l1);
+    ignore (List.map print_alpha_convertibility comparisons)
 end

f9ed10d25b5e915a4f2ab66cf1689434a4175dc4
Apolline Rodary <apolline.rodary@ens-lyon.fr>
2023-05-08 14:24:33 +0200

    Style change

--- bin/toplevel.ml
+++ bin/toplevel.ml
@@ -3,19 +3,20 @@ open Lambda
 
 let continue = ref true
 
-let () =
-print_endline "\r        Pieuvre Toplevel                    ";
-print_newline ();
+let () = begin
+    print_endline "\r        Pieuvre Toplevel                    ";
+    print_newline ();
 
-while (!continue) do
-    print_string "> ";
-    let s = read_line () in
-    match s with
-    | "exit" -> continue := false
-    | _ ->
-        begin
-            match (parse_string s) with
-            | Some x -> print_endline (string_of_lam x)
-            | None -> ()
-        end
-done
+    while (!continue) do
+        print_string "> ";
+        let s = read_line () in
+        match s with
+        | "exit" -> continue := false
+        | _ ->
+            begin
+                match (parse_string s) with
+                | Some x -> print_endline (string_of_lam x)
+                | None -> ()
+            end
+    done
+end

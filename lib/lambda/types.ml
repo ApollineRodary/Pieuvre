856916c051c6e0364e8d19a6d2127dd9d83a0321
thervier <thibault.hervier@ens-lyon.fr>
2023-05-16 10:46:06 +0200

    Definition of types And and Couple

--- lib/lambda/types.ml
+++ lib/lambda/types.ml
@@ -4,6 +4,7 @@ type ty =
     | TypeVar of var
     | Arrow of ty * ty
     | False
+    | And of ty * ty
 
 type lam =
     | Abstraction of var * ty * lam
@@ -11,5 +12,8 @@ type lam =
     | Var of var
     | Exf of lam * ty
     | Hole
+    | Couple of lam * lam
+    | Fst of lam
+    | Snd of lam
 
 type env = (var*ty) list

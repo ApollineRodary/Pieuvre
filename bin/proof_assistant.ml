66ed11eed1ad7ee767c23d6dae3e3a64c5c58d7c
Apolline Rodary <apolline.rodary@ens-lyon.fr>
2023-05-16 10:41:29 +0200

    Refactor proof assistant

--- bin/proof_assistant.ml
+++ bin/proof_assistant.ml
@@ -2,44 +2,50 @@ open Parsing
 open Lambda
 open Proof
 
+let read_prop () =
+    let request = parse (Lexing.from_channel stdin) (Parsing__Parser.property_request) in
+    Option.get request
+
+let read_and_apply_tactic (proof_state : (proof ref * string ref * bool ref)) =
+    let proof, message, continue = proof_state in
+    let (l, g) = !proof in begin
+        message := "";
+        Proof__Display.print_goals g;
+        print_lam l;
+        print_newline ();
+        print_newline ();
+    end;
+    try
+        let tactic = Option.get (parse (Lexing.from_channel stdin) (Parsing__Parser.ptactic)) in
+        proof := use_tactic tactic !proof
+    with
+    | Cannot_Apply_Tactic -> message := "\x1B[31mCould not apply tactic\x1B[39m\n\n"
+    | Incomplete_Proof -> message := "\x1B[31mThere are still subgoals to prove\x1B[39m\n\n"
+    | No_Goals_Left -> message := "\x1B[31mThere are no subgoals to apply this tactic on\x1B[39m\n\n"
+    | Proof_Admitted ->
+        begin
+            message := "\x1B[33mAdmitted\x1B[39m\n\n";
+            continue := false
+        end
+    | Proven ->
+        begin
+            message := "\x1B[32mSuccessfully proven\x1B[39m\n\n";
+            continue := false
+        end
+
 let start_proof () =
     ignore (Sys.command "clear");
 
-    let request = parse (Lexing.from_channel stdin) (Parsing__Parser.property_request) in
-    let prop = Option.get request
-    in begin
-        let proof = ref (proof_start prop) in
-        let message = ref "" in
-        let continue = ref true in
-        while (!continue) do 
-            let (l, g) = !proof in begin
-                ignore (Sys.command "clear");
-                print_string !message;
-                message := "";
-                Proof__Display.print_goals g;
-                print_lam l;
-                print_newline ();
-                print_newline ();
-            end;
-            try
-                let tactic = Option.get (parse (Lexing.from_channel stdin) (Parsing__Parser.ptactic)) in
-                proof := use_tactic tactic !proof
-            with
-            | Cannot_Apply_Tactic -> message := "Could not apply tactic\n"
-            | Incomplete_Proof -> message := "There are still subgoals to prove\n"
-            | No_Goals_Left -> message := "There are no subgoals to apply this tactic on\n"
-            | Proof_Admitted ->
-                begin
-                    message := "Admitted\n";
-                    continue := false
-                end
-            | Proven ->
-                begin
-                    message := "Proven\n";
-                    continue := false
-                end
-            | Invalid_argument e -> (*message := "Parsing error :(\n"*) raise (Invalid_argument e)
-            ;
+    let prop = read_prop () in
+    let proof = ref (proof_start prop)
+    and message = ref ""
+    and continue = ref true in
+    let proof_state = (proof, message, continue) in
+    begin   
+        while (!continue) do
+            ignore (Sys.command "clear");
+            print_string !message;
+            read_and_apply_tactic proof_state
         done;
         
         ignore (Sys.command "clear");
@@ -51,7 +57,6 @@ let start_proof () =
         begin if typecheck [] m prop then
             print_endline (string_of_lam m)
         else
-            print_endline "Failed to match lambda-term with the given lemma. This is expected if a goal was admitted, not so much otherwise."
+            print_endline "\x1B[33mFailed to match lambda-term with the given lemma. This is expected if a goal was admitted, not so much otherwise.\x1B[31m"
         end
-            
     end
